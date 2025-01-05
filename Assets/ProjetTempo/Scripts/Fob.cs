d3ae6a71c678407cf118cba1173bb605ebaa9a6d
Alba <>
2025-01-06 00:46:26 +0100

    Two levels, homepage

--- Assets/ProjetTempo/Scripts/Fob.cs
+++ Assets/ProjetTempo/Scripts/Fob.cs
@@ -1,37 +1,67 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class Fob : MonoBehaviour, IPointerClickHandler
 {
-public float speed = 1f;
+    public static UnityEvent onFinished = new();
+
+    public float speed = 1f;
+
+    // Count the number of Fob instances
+    static int count = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // add 1 to the count
+        count++;
+    }
+
+    // OnDestroy is called when the object is destroyed
+    void OnDestroy()
+    {
+        // remove 1 from the count
+        count--;
+
+        // if there are no more Fob instances
+        if (count == 0)
+        {
+            //Debug.Log("All Fobs are destroyed");
+
+            // call the onFinished event
+            onFinished.Invoke();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //move the object on z axis
+        // Move the object on z axis
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
-
-
-    
-        public void OnPointerClick(PointerEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)
     {
-
+        // if the distance between the object and the camera is less than 1.5
         if (Vector3.Distance(transform.position, Camera.main.transform.position) < 1f)
-
-        Catch();
+        {
+            Catch();
+        }
     }
 
-//this function is called when the object is clicked or touched
+    // This function is called when the object is clicked or touched
     public void Catch() {
         Debug.Log("Catch");
 
-//destroy the object
+        // destroy the object
         Destroy(gameObject);
     }
+
+    // Detect trigger collision
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("OnTriggerEnter with " + other.gameObject.name);
+
+        Destroy(gameObject);
+    }
+
 }
--- Assets/ProjetTempo/Scripts/FobSpawner.cs
+++ Assets/ProjetTempo/Scripts/FobSpawner.cs
@@ -2,55 +2,41 @@ using System.Linq;
 using UnityEngine;
 
 public class FobSpawner : MonoBehaviour
-
 {
     public GameObject[] fobPrefabs;
-
     public float spawnInitialDelay = 2f;
-
-    public float spawnDelay = 2f; //fobs every 2 seconds
-
+    public float spawnDelay = 2f; // fobs every 2 second
     public float initialSpeed = 1f;
-
     public int fobCount = 20;
-    public float spread = 1f;
-    public float verticalSpread = 1f;
     public float horizontalSpread = 1f;
+    public float verticalSpread = 1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //Call the Spawn function every 2 seconds
-        //InvokeRepeating("Spawn", spawnInitialDelay, spawnRate);
         Spawn();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void Spawn () {
 
-        //Spawn 10 Fobs along the z axis
-        for (int i = 0; i < fobCount; i++){
+        // spawn fobs along the spawner's z axis
+        for (int i = 0; i < fobCount; i++) {
 
             float distance = initialSpeed * spawnInitialDelay + i * initialSpeed * spawnDelay;
-            Vector3 position = transform.position + new Vector3(0, 0, distance);
+            Vector3 position = transform.position + transform.forward * distance;
 
-             //Instantiate a new Fob at the position of the spawner
-             GameObject fob = Instantiate(fobPrefabs[Random.Range(0, fobPrefabs.Count())], position, Quaternion.identity);
+            // Instantiate a new Fob at the position of the spawner
+            GameObject fob = Instantiate(fobPrefabs[Random.Range(0, fobPrefabs.Count())], position, Quaternion.identity);
 
-             //Look at the spawner
-             fob.transform.LookAt(transform);
+            // Look at the spawner
+            fob.transform.LookAt(transform);
 
-             //Add a random offset to the x and y axis
-             fob.transform.position += new Vector3(Random.Range(-horizontalSpread/2, horizontalSpread/2), Random.Range(-verticalSpread/2, verticalSpread/2), 0);
+            // Add a random offset to the x and y axis
+            fob.transform.position += new Vector3(Random.Range(-horizontalSpread/2, horizontalSpread/2), Random.Range(-verticalSpread/2, verticalSpread/2), 0);
 
-
-              //Set the speed of the fob
-             fob.GetComponent<Fob>().speed = initialSpeed;
+            // Set the speed of the fob
+            fob.GetComponent<Fob>().speed = initialSpeed;
         }
+
     }
 }
